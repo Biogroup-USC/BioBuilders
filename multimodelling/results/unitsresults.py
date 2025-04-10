260bc7a4b82c4fc23d5e74fabee237c8170a5794
Isaac <isaacleis.garrote@usc.es>
2025-04-10 16:37:42 +0200

    Changes in .gitmodules.

---
+++ multimodelling/results/unitsresults.py
@@ -0,0 +1,19 @@
+"""
+"""
+import pandas as pd
+import biosteam as bst
+
+class Display_Units_Results:
+    """
+    """
+    def __init__(self):
+        pass
+
+    def display_unit(self):
+        pass
+
+    def save_report(self):
+        pass
+
+    def display_costs(self):
+        pass
