6a2d60f6fbf0d49aa3d2cbf804d22ee3ddf08f06
Isaac <isaacleis.garrote@usc.es>
2025-05-07 17:31:31 +0200

    Fix the Assertion error cleaning the chemical_cache after every try

--- multimodelling/chems/newchem.py
+++ multimodelling/chems/newchem.py
@@ -1,7 +1,7 @@
 """
 """
-import thermosteam as tmo
 from .chem_db import ChemDataBase
+import biosteam as bst
 
 __all__=('ChemManager',)
 
@@ -33,7 +33,7 @@ class ChemManager:
         """
         return self.chems
     
-    def creating_chems(self, properties: dict = None):                          #TODO add the function to copy properties from a certain chemical
+    def creating_chems(self, properties: dict = None, checks = False):                          #TODO add the function to copy properties from a certain chemical
         """                                                                     
         This method allows creating any chemical disregarding wether or
         not it is yet defined in ChEDL database or it is not. The properties
@@ -76,79 +76,79 @@ class ChemManager:
         the different units inside MultiModelling.
         """
         # Create an empty Chemicals object (BioSteam) to append all the chemicals as Chemical objects (BioSteam)
-        chemicals = tmo.Chemicals([])
+        chemicals = bst.Chemicals([])
 
         #Create a dictionary to transformate the keys avoiding errors
         Transformed_Keys = {
             'Rho': 'rho','Default': 'default','Phase_ref': 'phase_ref','Phase_Ref': 'phase_ref',
             'Cache': 'cache','Formula': 'formula','Phase': 'phase','MW_g_per_mol': 'MW',
             'Cp_J_per_g_K': 'Cp','Rho_kg_per_m3': 'rho', 'Hvap_J_per_mol': 'Hvap','V_m3_per_mol': 'V',
+            'Hf_J_per_mol': 'Hf',
         }
+
+        # Download the Multimodelling database into %AppData%
+        DB = ChemDataBase().copy_multimodelling_db()
         
         for chem in self.chems:
             try:
-                Chemical = tmo.Chemical(
+                Chemical = bst.Chemical(
                         ID = chem,
                         search_db = True,
-                        db = 'ChEDL'
-                    )
-
-            except LookupError:
-                try:
-                    Chemical = tmo.Chemical(
-                        ID = chem,
-                        search_db = True,
-                        db = 'BioSTEAM'
+                        cache = False
                     )
                 
-                except LookupError:
-                    # Create a dictionary with the compulsory arguments         
-                    chem_args = {'ID': chem, 'search_db': False}                
+            except LookupError:
+                # Clean the chemical cache avoiding Assertion errors
+                bst.Chemical.chemical_cache.pop(chem, None)
+
+                # Create a dictionary with the compulsory arguments         
+                chem_args = {
+                    'ID': chem, 
+                    'search_db': False, 
+                    'cache': False,
+                    'Hf': 0
+                }                
                     
-                    # Check if the chemical exists in the multimodelling database
-                    DB = ChemDataBase()
-                    DB = DB.copy_multimodelling_db()
+                # Check if the chemical exists in the multimodelling database, If True get the properties
+                if DB.check_chemical(chem):
+                    Chem_Properties = DB.get_certain_data_from_db(chem,[
+                        "Rho","MW","formula","Phase","Hvap","Cp","V", "Hf"
+                    ])
 
-                    # If the chem is in the database, get its properties
-                    if DB.check_chemical(chem):
-                        Chem_Properties = DB.get_certain_data_from_db(chem,[
-                            "Rho","MW","formula","Phase","Hvap","V","Cp"
-                        ])
-
-                        for prop, value in Chem_Properties.items():
-                            key = Transformed_Keys.get(prop, prop)
-                            if isinstance(value,str):
-                                # Convert the numbers to float
-                                try:
-                                    chem_args[key] = float(value)
-                                except ValueError:
-                                    chem_args[key] = value
-                            else:
+                    for prop, value in Chem_Properties.items():
+                        key = Transformed_Keys.get(prop, prop)
+                        if isinstance(value,str):
+                            # Convert the numbers to float
+                            try:
+                                chem_args[key] = float(value)
+                            except ValueError:
                                 chem_args[key] = value
-                    # If the chem is not in any database, get the properties from the dictionary provided
-                    elif properties and chem in properties:
-                        # Add the properties defined in the input dictionary
-                        for prop, value in properties[chem].items():
-                            key = Transformed_Keys.get(prop, prop)
+                        else:
                             chem_args[key] = value
-                    else:
-                        # if the chemical is not in the ChEDL database, BioSTEAM database, multimodelling database and 
-                        # its properties are not defined, it gives back an error
-                        raise LookupError("The chemical {} properties must be provided".format(chem))
+                # If the chem is not in any database, get the properties from the dictionary provided
+                elif properties and chem in properties:
+                    # Add the properties defined in the input dictionary
+                    for prop, value in properties[chem].items():
+                        key = Transformed_Keys.get(prop, prop)
+                        chem_args[key] = value
+                else:
+                    # if the chemical is not in the ChEDL database, BioSTEAM database, multimodelling database and 
+                    # its properties are not defined, it gives back an error
+                    raise LookupError("The chemical {} properties must be provided".format(chem))
                     
-                    # Create the chemical using the chem_args
-                    Chemical = tmo.Chemical(**chem_args)
+                # Create the chemical using the chem_args
+                Chemical = bst.Chemical(**chem_args)
             
             # Add the chemical       
             chemicals.append(Chemical)                         
 
         #Compile all defined chemicals
-        chemicals.compile(skip_checks=True)
+        chemicals.compile(skip_checks=checks)
 
         # Return the Chemicals object from BioSteam
         return chemicals
     
-    def initialize_chemicals(self, chemicalsobj):
+    def initialize_chemicals(self, chemicalsobj, checks = False):
         """
         This method is used to initialize the thermo properties of the 
         chemicals.
@@ -159,7 +159,7 @@ class ChemManager:
         provided, this method initialize the thermodynamic property package.
 
         """
-        mixture = tmo.IdealMixture.from_chemicals(chemicalsobj)                     #TODO add the feature to use diferent models of mixtures
+        mixture = bst.IdealMixture.from_chemicals(chemicalsobj)                     #TODO add the feature to use diferent models of mixtures
         mixture.show()
-        tmo.settings.set_thermo(chemicalsobj,mixture=mixture,skip_checks=True)
-        tmo.settings.thermo.show()
+        bst.settings.set_thermo(chemicalsobj,mixture=mixture,skip_checks=checks)
+        bst.settings.thermo.show()
