32e8ebd1826ff89bc9fa194922a1dc353a763771
Isaac <isaacleis.garrote@usc.es>
2025-05-08 09:26:22 +0200

    Ignore test.py

--- multimodelling/test.py
+++
@@ -1,9 +0,0 @@
-
-import chems as cs
-import biosteam as bst
-import units as uts
-import results as rs
-import parameters as pts
-import tea 
-import biosteam as bst
-from biosteam.units.decorators import cost
