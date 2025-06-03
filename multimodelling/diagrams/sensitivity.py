f248bb0f26d6a3e9625c14992d801dc98d146653
Isaac <isaacleis.garrote@usc.es>
2025-06-03 14:14:01 +0200

    Add figsize_scale parameter
    The new figsize_scale parameter allows to scale the Spearman's plot depending on the amount of parameters of the model.

--- multimodelling/diagrams/sensitivity.py
+++ multimodelling/diagrams/sensitivity.py
@@ -7,9 +7,7 @@ __all__ = (
     "plot_spearman_1d",
 )
 
-def plot_spearman_1d(spearman_dataframe: pd.DataFrame = None, indicator: list | str = None):
-    """
-    """
+def plot_spearman_1d(spearman_dataframe: pd.DataFrame = None, indicator: list | str = None, figsize_scale: tuple = (0.75, 0.5)):
     """
     Plot Spearman correlation coefficients as horizontal bar charts.
 
@@ -21,6 +19,9 @@ def plot_spearman_1d(spearman_dataframe: pd.DataFrame = None, indicator: list | 
     indicator : str or list of str
         Name(s) of the indicator(s) (column) to plot. If a list is provided,
         a separate plot is generated for each indicator.
+    figsize_scale : float
+        scaling factor for figure size. Default is 0.5.
+
     """
     # Copy to avoid modifying caller's DataFrame
     Df = spearman_dataframe.copy()
@@ -45,7 +46,8 @@ def plot_spearman_1d(spearman_dataframe: pd.DataFrame = None, indicator: list | 
         Coeffs = Coeffs.reindex(Coeffs.abs().sort_values(ascending=False).index)
 
         # Plot settings
-        Fig, Ax = plt.subplots(figsize=(8, max(4, 0.4 * len(Coeffs))))
+        N_Vars = Df[ind].shape[0]
+        Fig, Ax = plt.subplots(figsize=(N_Vars * figsize_scale[0], N_Vars * figsize_scale[1]))
         Ax.barh(Coeffs.index, Coeffs.values, color='skyblue', edgecolor='k')
         Ax.invert_yaxis()
         Ax.axvline(0, color='red', linewidth=0.8)
