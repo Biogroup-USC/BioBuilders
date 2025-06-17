72650721f7fa8aee591eeffcbc360e3aaf18e26b
Isaac <isaacleis.garrote@usc.es>
2025-06-17 16:44:59 +0200

    Refactor SLEPFbySplit class documentation (WIP); add design and cost methods (WIP)

--- multimodelling/units/extraction.py
+++ multimodelling/units/extraction.py
@@ -20,28 +20,41 @@ class SLEPFbySplit(bst.Unit):
     it is important to remark that the amount of washing stream used in the filtration, which is 
     retained by te solid, is calculated using the moisture parameter.           
 
-    - ID (str): Name of the unit.
-
-    - ins (list): List of input streams (BioSTEAM object). This unit has 3 inputs. Note that the Filter_Washing stream must be an empty one.
-    [Feed, Solvent, Filter_Washing]
-
-    - outs (list): List of output streams (BioSTEAM object). This unit has 2 outputs. [Extract, Raffinate]
-
-    - sfi (dict): Dictionary with the following structure: {"A": 0.5, "B": 0.9}. This means that 50% of A and 90% of B will be extracted by the 
-    solvent.
-
-    - moisture_content (float): percentage of washing stream retained in the solids. Default to 0.40 kg of washing stream per kg of dry solids.
-
-    - washing_chem (dict): Dictionary with the following structure:  {"C": 2, "D": 4}. This means that the steam used to wash the solids during the filtration
-    will have a flow of C and D which is 2 and 4 times the flow of Feed, respectively.
-
-    - tau (float): Residence time in h.
-
-    - kW_per_m3 (float): The power consumption due to stirring. The default is set to 0.1803 kW/m3 using the Piccino calculation for 1 m3 reactor: 
-    http://dx.doi.org/10.1016/j.jclepro.2016.06.164.
-
-    - operating_T (float): The operating temperature is set to 298.15 K by default.
-
+    Parameters
+    ----------
+    ID : (str)
+        Name of the unit.
+    ins : (list)
+        List of input streams (BioSTEAM object). This unit has 3 inputs.[Feed, Solvent, Filter_Washing]
+    outs : (list)
+        List of output streams (BioSTEAM object). This unit has 2 outputs. [Extract, Raffinate]
+    sfi : (dict)
+        Dictionary with the following structure: {"A": 0.5, "B": 0.9}. This means that 50% of A and 90% of B will be 
+        extracted by the solvent.
+    moisture_content : (float)
+        percentage of washing stream retained in the solids. Default to 0.40 kg of washing stream per kg of dry solids.
+    washing_chem : (dict)
+        Dictionary with the following structure:  {"C": 2, "D": 4}. This means that the steam used to wash the solids 
+        during the filtration will have a flow of C and D which is 2 and 4 times the flow of Feed, respectively.
+    tau : (float) 
+        Residence time in h.
+    operating_T : (float)
+        The operating temperature is set to 298.15 K by default.
+    
+    Attributes
+    ----------
+    kW_per_m3 : (float)
+        The power consumption due to stirring. The default is set to 0.1803 kW/m3 using the Piccino calculation for 1 m3 
+        reactor: http://dx.doi.org/10.1016/j.jclepro.2016.06.164.
+    base_cost_tank : float
+        Base cost of a jacketed mixing tank.
+    base_volume_tank : float
+        The volume of the jacketed mixing tank whose cost is the base cost.
+    base_n_cost_tank : float
+        Parameter n for the jacketed mixing tank used in the formula to scale costs.
+    CE_base_tank : float
+        CEPCI of the base cost.
+        
     """
     # Inlets
     _N_ins = 3
@@ -50,37 +63,38 @@ class SLEPFbySplit(bst.Unit):
     _N_outs = 2
 
     # Set the default moisture content (kg washing stream / kg dry solids)
-    moisture_content_default: Optional[float] = 0.40
+    moisture_content_default: float = 0.40
 
     # Set the default washing chem and its kg / kg solids 
     wash_chem_default = {'Ethanol': 80}                                   #TODO ITBQ is probably not using pure ethanol --> get more info about it for setting the default
 
     # Default operating temperature (k)
-    T_default: Optional[float] = 273.15 + 25
+    T_default: float = 273.15 + 25
 
-    # Default residence time (tau)
-    tau_default: Optional[float] = None
-
-    # Default power consumption by agitation (kW/m3)
-    kW_per_m3_default: Optional[float] = (0.79*1000*(1.417**3)*(0.373**5))/90      # using 1 m3 data --> http://dx.doi.org/10.1016/j.jclepro.2016.06.164
+    # Default residence time (h)
+    tau_default: float = 0.5
 
     def _init(self,
               sfi: dict = None,
               moisture_content: float = None,
               washing_chem: dict = None,
               tau: float = None,
-              kW_per_m3: Optional[float] = None,
-              operating_T: Optional[float] = None
+              operating_T: float = None,
+              kW_per_m3: float = None,
               ):
         """
         """
         self.sfi = sfi
-        self.moisture = self.moisture_content_default if moisture_content is None else moisture_content
-        self.wash_chem = self.wash_chem_default if washing_chem is None else washing_chem
-        self.tau = self.tau_default if tau is None else tau
-        self.operating_T = self.T_default if operating_T is None else operating_T
-        self.kW_per_m3 = self.kW_per_m3_default if kW_per_m3 is None else kW_per_m3
-        
+        self.moisture = moisture_content if moisture_content is not None else self.moisture_content_default
+        self.wash_chem = washing_chem if washing_chem is not None else self.wash_chem_default
+        self.tau = tau if tau is not None else self.tau_default
+        self.operating_T = operating_T if operating_T is not None else self.T_default
+        self._kW_per_m3 = kW_per_m3
+        self._base_cost_tank = None
+        self._base_volume_tank = None
+        self._base_n_cost_tank = None
+        self._CE_base_tank = None
+
     def _run(self):
         """
         """
@@ -97,42 +111,142 @@ class SLEPFbySplit(bst.Unit):
         Extract = self.outs[0]
         Raffinate = self.outs[1]
 
-        # Run the solid liquid extraction using split factors
+        # The mixture of streams is simulated in the design section to perform the heat utilities.
+        # The mix is simulated here copying the Feed and Solvent streams as Raffinate and Extract
         Extract.copy_flow(Solvent)
         Extract.T = self.operating_T
         Raffinate.copy_flow(Feed)
         Raffinate.phase = Feed.phase
         Raffinate.T = self.operating_T
+
+        # Simulate the separation using a pressure filter
         for chem in self.sfi.keys():
             Extract.imass[chem] = self.sfi[chem] * Feed.imass[chem] + Solvent.imass[chem]   # It takes into account if there is any chemical in both Solvent and Feed
             Raffinate.imass[chem] = (1-self.sfi[chem])* Feed.imass[chem]                    # assuming all the solvent and solutes are washed during the filtration 
         
-        # Run the pressure filtration assuming all the solvent phase is washed from the solids
-        ## Calculate the remaining chemical from Filter_Washing stream in the solids using the moisture
-        ### Create a mock stream to manage the retained chemicals
-        Washing_Fluid_Retained = bst.Stream('Mock')
-        Washing_Fluid_Retained.copy_like(Filter_Washing)
+        # Calculate the amount of solvent retained
+        solvent_retained = self.moisture * Raffinate.F_mass
+        solvent_retained_ratio = solvent_retained/Solvent.F_mass
+        for chemobj in Solvent.available_chemicals:
+            chem = chemobj.ID
+            Extract.imass[chem] = (1-solvent_retained_ratio) * Solvent.imass[chem]
+            Raffinate.imass[chem] = solvent_retained_ratio * Solvent.imass[chem]
+            # Check if there is enough solvent to match the moisture content
+            solvent_extract_plus_raffinate = (Extract.imass[chem] + Raffinate.imass[chem])
+            if not np.isclose(solvent_extract_plus_raffinate, Solvent.imass[chem], rtol = 1e-5, atol = 1e-8):
+               raise ValueError("There is not enough amount of {} in {} to match the moisture requeriments".format(chem, Solvent.ID))
 
-        ### Retained flow as RF = moisture * Feed (Dry solids)
-        Retained_Flow = self.moisture * Raffinate.get_total_flow('kg/hr')
+    @property
+    def kW_per_m3(self):
+        """
+        """
+        if self._kW_per_m3 is None:
+            self._kW_per_m3 = (0.79*1000*(1.417**3)*(0.373**5))/90  # using 1 m3 data --> http://dx.doi.org/10.1016/j.jclepro.2016.06.164
+        return self._kW_per_m3
+    
+    @kW_per_m3.setter
+    def kW_per_m3(self,value):
+        """
+        """
+        self._kW_per_m3 = value
 
-        ### The retained ratio is calculated as (retained flow)/(total washing flow)
-        Retained_Ratio = Retained_Flow/Filter_Washing.get_total_flow('kg/hr')
+    @property
+    def V_max(self):            # This value is selected because the range of the cost correlation is 3 - 90 m3
+        """
+        """
+        if self._V_max is None:
+            self._V_max = 80    #m3  
+        return self._V_max
+    
+    @V_max.setter
+    def V_max(self,value):
+        """
+        """
+        self._V_max = value
 
-        ### Get the amount of each chemical retained 
-        Washing_Fluid_Retained.F_mass = Filter_Washing.F_mass * Retained_Ratio
-        
-        ## Once the amount of retained washing stream is calculated, the flow of each chemical is set in both outlets
-        for chem in self.wash_chem.keys():
-            Raffinate.imass[chem] = Washing_Fluid_Retained.imass[chem]
-            Extract.imass[chem] = Filter_Washing.imass[chem] - Raffinate.imass[chem] + Extract.imass[chem]
-
-            # Check if there is enough amount in the Filter_Washing stream to satisfy the moisture content
-            if Extract.imass[chem] <= 0:
-                raise ValueError("The amount of {} from {} is not enough to achieve the moisture provided {}". format(chem, Filter_Washing.ID, self.moisture))
+    @property
+    def V_wf(self):
+        """
+        """
+        if self._V_wf is None:
+            self._V_wf = 0.80
+        return self._V_wf
+    
+    @V_wf.setter
+    def V_wf(self,value):
+        """
+        """
+        self._V_wf = value
 
     def _design(self):
-        pass
+        """
+        """
+        # Load the dictionary of results
+        design = self.design_results
+
+        # Load the input streams of the unit and mix them
+        Ins1 = self.ins[0]
+        Ins2 = self.ins[1]
+        Load = bst.Stream(units = 'kg/hr')
+        Load.mix_from([Ins1,Ins2], energy_balance = True)
+
+        # Load the parameters
+    
+    @property
+    def base_cost_tank(self):
+        """
+        """
+        if self._base_cost_tank is None:
+            self._base_cost_tank = 75000     # USD
+        return self._base_cost_tank   
+
+    @base_cost_tank.setter
+    def base_cost_tank(self, value):
+        """
+        """
+        self._base_cost_tank = value
+
+    @property
+    def base_volume_tank(self):
+        """
+        """
+        if self._base_volume_tank is None:
+            self._base_volume_tank = 3.0    # m3
+        return self._base_volume_tank
+    
+    @base_volume_tank.setter
+    def base_volume_tank(self, value):
+        """
+        """
+        self._base_volume_tank = value
+
+    @property
+    def base_n_cost_tank(self):
+        """
+        """
+        if self._base_n_cost_tank is None:
+            self._base_n_cost_tank = 0.53
+        return self._base_n_cost_tank
+    
+    @base_n_cost_tank.setter
+    def base_n_cost_tank(self, value):
+        """
+        """
+        self._base_n_cost_tank = value
+    
+    @property
+    def CE_base_tank(self):
+        """
+        """
+        if self._CE_base_tank is None:
+            self._CE_base_tank = 1000.0
+        return self._CE_base_tank
+    
+    @CE_base_tank.setter
+    def CE_base_tank(self, value):
+        """
+        """
+        self._CE_base_tank = value
 
     def _cost(self):
         pass
@@ -150,33 +264,24 @@ class SLECbySplit(bst.Unit):
     ----------
     ID : str
         Name of the unit.
-    
     ins : list 
         List of input streams (BioSTEAM object). This unit has 2 inputs. [Feed, Solvent]
-    
     outs : list 
         List of output streams (BioSTEAM object). This unit has 2 outputs. [Extract, Raffinate]
-    
     sfi : dict 
         Dictionary with the following structure: {"A": 0.5, "B": 0.9}. This means that 50% of A and 90% of B will be extracted by the 
         solvent.  
-
     moisture_content : float 
         Percentage of solvent retained in the solids. Default to 0.40 kg of solvent per kg of dry solids.
-
     tau : float
         Residence time in h.
-
     operating_T : float
         Operating temperature of this unit. Default to 298.15 K.
-    
     operating_P : float
         Operating pressure of this unit. Default to 101325 Pa.
-    
     solids_rho : float
         Density of the solids [kg/m3] to calculate the centrifuge diameter. This parameter allows to give a density for
         the solids that enters the solid-liquid extraction manually instead of calculating it using the BioSTEAM features.
-    
     solvent_rho : float
         Density of the solvent [kg/m3] to calculate the centrifuge diameter. This parameter allows to give a density for
         the solids that enters the solid-liquid extraction manually instead of calculating it using the BioSTEAM features.
@@ -186,49 +291,34 @@ class SLECbySplit(bst.Unit):
     kW_per_m3_reactor : float 
         The power consumption due to stirring. The default is set to 0.1803 kW/m3 using the Piccino calculation for 1 m3 reactor: 
         http://dx.doi.org/10.1016/j.jclepro.2016.06.164.
-    
     kW_per_kg_centrifuge : float
         The power consumption of the centrifuge. The default is 0.010 kWh/ton.
-
     V_wf : float
         The working volume parameter represents the fraction of the reactor filled. Defaults to 0.80.
-    
     V_max : float
         The maximum volume (m3) represents the limit of the tank dimension. Default to 80 m3. 
-
     max_diameter : float
         The maximum diameter of the centrifuge based on the cost correlations. Default to 1.25 m.
-    
     particle_diameter : float
         Diameter of the solids. Default to 5e-4 m.
-    
     centrifuge_rpm : float
         Rotational speed of the centrifuge. Default to 3000 rpm.
-
     centrifuge_height : float
         Height of the centrifuge' basket. Defaulto to 1 m.
-        
     base_cost_tank : float
         Base cost of a jacketed mixing tank.
-    
     base_volume_tank : float
         The volume of the jacketed mixing tank whose cost is the base cost.
-    
     base_n_cost_tank : float
         Parameter n for the jacketed mixing tank used in the formula to scale costs.
-    
     CE_base_tank : float
         CEPCI of the base cost.
-    
     base_cost_centrifuge : float
         Base cost of a basket centrifuge.
-    
     base_diameter_centrifuge : float
         The diameter of a basket centrifuge whose cost is the base cost.
-    
     base_n_cost_centrifuge : float
         Parameter n for the basket centrifuge used in the formula to scale costs.
-    
     CE_base_centrifuge : float
         CEPCI of the base cost.
              
