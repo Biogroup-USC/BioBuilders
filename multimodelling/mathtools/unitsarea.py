3ba140dce1022a8487888f3a3a51415f0aff47ef
Isaac <isaacleis.garrote@usc.es>
2025-07-22 14:52:57 +0200

    Rework of the function to calculate the drum rotatory filter area; Delete some functions not needed

--- multimodelling/mathtools/unitsarea.py
+++ multimodelling/mathtools/unitsarea.py
@@ -1,8 +1,10 @@
 """
 """
 
+import math
+
 __all__ = (
-    "calculate_rdvf_area"
+    "calculate_rdvf_area",
 )
 
 Filtration_Data = {                         # Reference: Rules of the Thumb in Engineering Practice: page 168 / DOI: 10.1002/9783527611119.
@@ -67,55 +69,30 @@ def cakeweight_formtime_correlation(
     else:
         raise ValueError("A value for either cakeweight or formtime must be provided")
 
-def calculate_theta_from_thickness(
-        L: float,
-        epsilon: float,
-        rho_s: float,
-        Cs: float
-):
-    """
-    """
-    if not (0 < epsilon < 1): 
-        raise ValueError("Porosity (epsilon) must remain between 0 and 1.")
-    return (1-epsilon) * rho_s * L / Cs
-
-def calculate_theta_from_rate(
-        v_cake: float,
-        omega: float,
-        epsilon: float,
-        rho_s: float,
-        Cs: float,
-        submergence: float = 0.35      
-):
-    """
-    """
-    if omega <= 0:
-        raise ValueError("Angular velocity must be > 0.")
-    t_sub = submergence / omega
-    L = v_cake * t_sub
-    return calculate_theta_from_thickness(L,epsilon,rho_s,Cs)
-
 def calculate_rdvf_area(
         mf_fil: float,
         rho_fil: float,
         delta_P: float,
         mu: float,
-        alpha: float,
+        alpha: float|str,
         Cs: float,
-        theta: float,
-        Rm: float|str,
         submergence: float = 0.35,
              
 ):
     """
 
     Calculate the filtration area required for a rotatory drum vacuum
-    filter given the filtrate rate (Q), pressure difference 
-    (delta_P), viscosidad (mu), Filtrate volume per m2 (theta), medium 
-    resistence (Rm) and cake resistence (alpha). The equation used is 
+    filter given the filtrate rate (Q), pressure difference (delta_P), 
+    viscosidad (mu) and cake resistence (alpha). The equation used is 
     the following:
 
-    Q/area = delta_P/(mu*(Rm + alpha * Cs * theta))
+    Q/area = delta_P/(mu*(Rm + alpha * Cs * V/A))
+
+    which is simplified assuming that cake resistence is significantly 
+    bigger than the filtration media resistence, obtaining the expression
+    below.
+
+    A = ((Q*mu*alpha*Cs)/delta_P)**0.5
 
     Parameters
     ----------
@@ -125,40 +102,30 @@ def calculate_rdvf_area(
 
     mu : float
 
-    alpha : float
+    alpha : float | str
 
     Cs : float
-
-    theta : float
-
-    Rm : float|str
     
     submergence : float
 
     """
-    if isinstance(Rm, str) and Rm in Filtration_Media_Resistence:
-        Rm_val = Filtration_Media_Resistence[Rm]
-    elif isinstance(Rm, (int, float)):
-        Rm_val = Rm
-    else:
-        raise ValueError("Rm must be either a float or one of the following str: {}".format(Filtration_Media_Resistence.keys()))
-
     # Conversion from mass flow (kg/h) to volumetric flow (m3/s)
     if rho_fil <= 0:
-        raise ValueError("La densidad del filtrado (rho_f) debe ser mayor que 0.")
+        raise ValueError("Filtrate density must be > 0.")
     Q = mf_fil / rho_fil / 3600
 
-    denominator = mu * (alpha * Cs * theta + Rm_val)
-    if denominator <= 0:
-        raise ValueError("Invalid denominator. Check the following parameters: mu, Rm and theta")
+    # Calculate the effective area
+    if isinstance(alpha,str):
+        alpha = Filtration_Data[alpha]["Cake resistance"]
+    numerator = Q * mu * alpha * Cs
+    denominator = delta_P
 
-    A_effective = Q * denominator/delta_P
+    A_effective = math.sqrt(numerator/denominator)
 
+    # Calculate the total area
     if submergence <= 0:
         raise ValueError("Submergence must be between 0 and 1.")
     
     A_total = A_effective/submergence
 
     return A_total
-
-print(calculate_rdvf_area(631.4,1000,50000,0.001,8e10,100,calculate_theta_from_rate(0.001,2.67,0.4,1000,100),"PI"))
