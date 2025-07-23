57a2afea319f58c589f15993176064a0628c6c6d
Isaac <isaacleis.garrote@usc.es>
2025-07-23 17:11:47 +0200

    Adapt SolidsSeparator from BioSTEAM to include the cost calculation parameters in RotatoryVacuumFilter

--- multimodelling/units/filtration.py
+++ multimodelling/units/filtration.py
@@ -4,9 +4,79 @@
 import biosteam as bst
 import numpy as np
 
+__all__ = (
+    'RotaryVacuumFilter',
+)
+
 # Code adapted from BioSTEAM (https://biosteam.readthedocs.io/), under the University of Illinois/NCSA Open Source License
 # Copyright (c) 2019-2023 BioSTEAM Development Group. All rights reserved.
-class RotaryVacuumFilter(bst.SolidsSeparator):
+class SolidsSeparator(bst.Splitter):
+    """
+    Create SolidsSeparator object.
+    
+    Parameters
+    ----------
+    ins : 
+        Inlet fluids with solids.
+    outs : 
+        * [0] Retentate.
+        * [1] Permeate.
+    split : array_like
+        Component splits to 0th output stream
+    moisture_content : float
+        Fraction of water in solids
+    
+    """
+    _N_ins = 1
+    _ins_size_is_fixed = False
+    
+    def _init(self, split, 
+            order=None, moisture_content=None, 
+            moisture_ID=None,
+            strict_moisture_content=None
+        ):
+        bst.Splitter._init(self, order=order, split=split)
+        #: Moisture content of retentate
+        self.moisture_content = moisture_content
+        self.strict_moisture_content = strict_moisture_content
+        if moisture_content is not None:
+            if moisture_ID is None: moisture_ID = '7732-18-5'
+            self.moisture_ID = moisture_ID
+        self._base_cost_filter = None
+        self._base_area_filter = None
+        self._base_n_cost_filter = None
+        self._CE_base_filter = None
+    
+    def _run(self):
+        if self.moisture_content is None:
+            bst.separations.mix_and_split(
+                self.ins, *self.outs, self.split,
+            )
+        else:
+            moisture_ID = self.moisture_ID
+            self.isplit[moisture_ID] = 0.
+            bst.separations.mix_and_split_with_moisture_content(
+                self.ins, *self.outs, self.split, self.moisture_content, self.moisture_ID,
+                self.strict_moisture_content,
+            )
+
+
+    #     if self._recycle_system and self._system.algorithm == 'Phenomena oriented':
+    #         ID = self.moisture_ID
+    #         if not ID: return
+    #         top, bottom = self.outs
+    #         top_mol = top.imol[ID]
+    #         self.isplit[ID] = top_mol / (top_mol + bottom.imol[ID])
+            
+    # def _update_nonlinearities(self):
+    #     outs = self.outs
+    #     data = [i.get_data() for i in outs]
+    #     self._run()
+    #     for i, j in zip(outs, data): i.set_data(j)
+
+# Code adapted from BioSTEAM (https://biosteam.readthedocs.io/), under the University of Illinois/NCSA Open Source License
+# Copyright (c) 2019-2023 BioSTEAM Development Group. All rights reserved.
+class RotaryVacuumFilter(SolidsSeparator):
     """
     Create a RotaryVacuumFilter object.
     
@@ -44,10 +114,10 @@ class RotaryVacuumFilter(bst.SolidsSeparator):
     _bounds = {'Individual area': (2, 100)}
     _units = {'Area': 'm^2',
               'Individual area': 'm^2'}
-    
+
     def _design(self):
         flow = sum([stream.F_mass for stream in self.outs])
-        self.design_results['Area'] = self._calc_Area(flow, self.filter_rate)
+        self.design_results['Area'] = self._calc_Area(flow, self.filter_rate) * 0.092903
 
     @property
     def base_cost_filter(self):
