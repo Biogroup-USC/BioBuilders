6fdd7e3168ef8519135bd301c86ac8ddc2326de9
Isaac <isaacleis.garrote@usc.es>
2025-10-27 17:02:29 +0100

    0.28.0 Release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multimodelling',
-    version='0.27.25',
+    version='0.28.0',
     author= "Isaac",
     author_email= "isaacleis.garrote@usc.es",
     description = 'Library for Biorefinery modelling following the Multiscale Approach using BioSTEAM as a Framework',  
