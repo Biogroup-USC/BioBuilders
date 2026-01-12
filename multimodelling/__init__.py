bacb9f7a2df46978fcbf43ebe3d568e3a591d089
Isaac <isaacleis.garrote@usc.es>
2026-01-12 16:26:13 +0100

    Refactor: encapsulate process settings logic in ProcessSettingsManager

--- multimodelling/__init__.py
+++ multimodelling/__init__.py
@@ -6,7 +6,7 @@ from .units import (
 from .chems import ChemDataBase, ChemicalsManager
 from .parameters import get_parameters_from_CSV, get_parameters_from_excel, get_price_streams_from_CSV, get_unit_costs_from_CSV
 from .results import DisplayMassResults, ProcessMassBalance, DisplayUnitsResults, ResultsTEA
-from .tea import load_process_settings, TEA, InflationTEA
+from .tea import ProcessSettingsManager, TEA, InflationTEA
 from .tools.mathtools import log_mean, discounting_to_present_value, updating_to_future_value, calculate_centrifuge_diameter, solve_operating_pressures_multieffectevaporator, calculate_labor_requirements, calculate_rdvf_area, damp_to, calculate_mean_median_price
 from .diagrams import UncertaintyPlotter, plot_spearman_1d, ContourStudy
 from .sensitivity import SRC
@@ -16,7 +16,7 @@ __all__ = (
     'Flash', 'RotaryVacuumDrumFilter','RotaryVacuumFilter', 'LLEbySplit' ,'ShellHeatExchanger', 'SLECbySplit', 'ExtractionReactor', 'BatchEnzymaticTreatment', 
     'BinaryDistillation','Mill', 'SolidsCentrifuge','DrumDryer', 'MultiEffectEvaporator', 'MixTank', "ContinuousStirredTankReactor", 'ChemDataBase', 'ChemicalsManager',
     'get_parameters_from_CSV', 'get_parameters_from_excel', 'get_price_streams_from_CSV','get_unit_costs_from_CSV', 'DisplayMassResults', 'DisplayUnitsResults', 
-    'ResultsTEA', 'load_process_settings', 'TEA', 'log_mean','discounting_to_present_value','updating_to_future_value','calculate_centrifuge_diameter',
+    'ResultsTEA', 'ProcessSettingsManager', 'TEA', 'log_mean','discounting_to_present_value','updating_to_future_value','calculate_centrifuge_diameter',
     'solve_operating_pressures_multieffectevaporator', 'calculate_labor_requirements', 'calculate_rdvf_area', 'UncertaintyPlotter','plot_spearman_1d',
     'SRC', 'LLESettler', 'LiquidsSettler', 'InflationTEA', 'damp_to', 'ContourStudy', 'ProcessMassBalance', 'calculate_agitator_power', 'extract_components_flow',
     'MembraneFiltration', 'calculate_centrifuge_diameter', 'SeparationUnit', 'calculate_mean_median_price', 'calculate_stream_price', 'main_chemical_mass_basis',
