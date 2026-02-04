9981875edf8e3ef07d3fd5501e0b03ea20c3d56f
Isaac Leis Garrote <isaacleis.garrote@usc.es>
2026-02-04 15:21:44 +0100

    Rename package from multimodelling to bravas

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='multimodelling',
+    name='bravas',
     version='0.29.9',
-    author= "Isaac",
+    author= "Isaac, Andrea",
     author_email= "isaacleis.garrote@usc.es",
-    description = 'Library for Biorefinery modelling following the Multiscale Approach using BioSTEAM as a Framework',  
+    description = 'Library to perform biorefinery and value chain assestment',  
     long_description = open("README.md").read(),
     long_description_content_type= "text/markdown",
-    url = "https://github.com/Biogroup-USC/multiscale-modelling-biorefinery-code.git", 
+    url = "https://github.com/Biogroup-USC/BRAVAS.git", 
     packages = find_packages(),
     include_package_data = True,
     package_data={
