42b0981f485a0a199f57921ba98e51fd7a92cad9
Isaac <isaacleis.garrote@usc.es>
2026-02-23 17:56:12 +0100

    Release 0.32.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bravas',
-    version='0.31.7',
+    version='0.32.0',
     author= "Isaac, Andrea",
     author_email= "isaacleis.garrote@usc.es",
     description = 'Library to perform biorefinery and value chain assestment',  
