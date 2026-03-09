18fb5628e433b3178fc71c9f6a9456033f515bf9
Isaac <isaacleis.garrote@usc.es>
2026-03-09 18:44:37 +0100

    Create Chemicals_Manager structure (WIP)

---
+++ bravas/chems/manager.py
@@ -0,0 +1,25 @@
+from biosteam import Chemical
+
+class ChemicalsManager:
+    """
+    """
+    def __init__(self, chemical_list, storage):
+        self.chemical_list = chemical_list
+
+    def append_chemical(self, chemical: str | Chemical):
+        pass
+
+    def remove_chemical(self, chemical: str | Chemical):
+        pass
+
+    def get_chemical_object(self, name):
+        pass
+
+    def get_chemicals_object(self):
+        pass
+
+    def compile_chemicals(self):
+        pass
+
+    def set_thermodynamics(self):
+        pass
