230e467c589b11284bb583c75592e1811fbbc8cd
Isaac <isaacleis.garrote@usc.es>
2026-03-09 18:28:28 +0100

    Add custom exceptions for chemicals database

--- bravas/chems/exceptions.py
+++ bravas/chems/exceptions.py
@@ -1,30 +1,37 @@
-"""
-"""
-from __future__ import annotations
+__all__ = (
+    "DuplicateChemicalError",
+    "ChemicalNotFoundError",
+)
 
 class BRAVASChemError(Exception):
-    """Base class for all BRAVAS chemical database / connector errors"""
-
-class ChemicalNotFound(BRAVASChemError, LookupError):
     """
     
-    Raised when a chemical cannot be resolved in:
-    - User DB
-    - BRAVAS embedded DB
-    - BioSTEAM/ThermoSTEAM/ChEDL
+    Base class for all BRAVAS chemical-related errors.
 
     """
-    def __init__(self, identifier: str, where: str | None = None):
-        self.identifier = identifier
-        self.where = where
-        msg = f"Chemical '{identifier}' was not found"
-        if where:
-            msg += f" ({where})"
+    pass
+
+class DuplicateChemicalError(BRAVASChemError):
+    """
+    
+    Raised when attempting to store a chemical that already exists
+    in the user chemical database.
+
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+        msg = f"Chemical '{name}' already exists in the database."
         super().__init__(msg)
 
-class UnsupportedProperty(BRAVASChemError, KeyError):
+class ChemicalNotFoundError(BRAVASChemError):
     """
     
-    
+    Raised when a chemical does not exist in the user chemical database.
 
     """
+
+    def __init__(self, name: str):
+        self.name = name
+        msg = f"Chemical '{name}' not found in the user chemical database."
+        super().__init__(msg)
