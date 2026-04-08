4563566e15adf5aa2af83ca0fc839aef2420c63a
Isaac <isaacleis.garrote@usc.es>
2026-04-08 15:59:18 +0200

    Refactor ChemicalsManager; Add option to add or remove chemicals

--- bravas/chems/manager.py
+++ bravas/chems/manager.py
@@ -6,9 +6,12 @@ from .exceptions import ChemicalNotFoundError
 class ChemicalsManager:
     """
     """
-    def __init__(self, chemical_list: Iterable[str | Chemical], storage: UserChemicalStorage = None):
+    def __init__(self, chemical_list: Iterable[str | Chemical], storage: UserChemicalStorage = None, skip_checks: bool = False):
         self.chemical_list = list(chemical_list)
         self.db = storage if storage is not None else UserChemicalStorage()
+
+        self._compiled_chemicals = None
+        self._skip_checks = skip_checks
 
     def _resolve_chemical_from_storage(self, chemical_id: str) -> Chemical:
         """
@@ -23,11 +26,17 @@ class ChemicalsManager:
         else:
             aliases = sorted(record.aliases)
 
+        if record.copy_missing_from.strip().lower() == "water":
+            default = True
+        else:
+            default = False
+
         chemical = Chemical(
             ID = record.name,
             CAS = record.CAS,
             search_ID = False,
             search_db = False,
+            default = default,
             MW = record.MW,
             formula = record.formula,
             aliases = aliases,
@@ -44,7 +53,7 @@ class ChemicalsManager:
         try:
             chemical = Chemical(chemical_id)
             return chemical
-        except LookupError as e:
+        except LookupError:
             raise ChemicalNotFoundError(chemical_id,'BioSTEAM/ChEDL')
     
     def get_chemical_object(self, chemical: str | Chemical) -> Chemical:
@@ -64,25 +73,76 @@ class ChemicalsManager:
             chem = self.get_chemical_object(chemical)
             if chem.ID not in seen:
                 chemicals_list.append(chem)
-                seen.add(chem)
+                seen.add(chem.ID)
 
         return Chemicals(chemicals_list)
 
-    def compile_chemicals(self, skip_checks: bool = False):
+    def _compile_chemicals(self):
         chemicals = self.get_chemicals_object()
-        chemicals.compile(skip_checks=skip_checks)
+        chemicals.compile(skip_checks=self.skip_checks)
         return chemicals
 
+    @property
+    def skip_checks(self):
+        return self._skip_checks
+
+    @skip_checks.setter
+    def skip_checks(self, value: bool):
+        value = bool(value)
+        if value != self._skip_checks:
+            self._skip_checks = value
+            self.reset_chemicals()
+
+    @property
+    def compiled_chemicals(self):
+        if self._compiled_chemicals is None:
+            self._compiled_chemicals = self._compile_chemicals()
+        return self._compiled_chemicals
+
+    def reset_chemicals(self):
+        self._compiled_chemicals = None
+
+    def add_chemical(self, chemical: str | Chemical):
+        self.chemical_list.append(chemical)
+        self.reset_chemicals()
+
+    def remove_chemical(self, chemical: str | Chemical):
+        if isinstance(chemical, str):
+            chemical_id = chemical
+        elif isinstance(chemical, Chemical):
+            chemical_id = chemical.ID
+        else:
+            raise ValueError(
+                f"Only 'str' or 'bst.Chemical' is supported."
+            )
+        
+        new_list = []
+        removed = False
+
+        for chem in self.chemical_list:
+            current_id = chem if isinstance(chem, str) else chem.ID
+            if current_id == chemical_id and not removed:
+                removed = True
+                continue
+            new_list.append(chem)
+        
+        if not removed:
+            raise ChemicalNotFoundError(chemical_id)
+        
+        self.chemical_list = new_list
+        self.reset_chemicals()
+
     def set_thermodynamics(
-            self, 
-            skip_checks: bool = False, 
+            self,
             mixture_class: Mixture = IdealMixture, 
             activity_coeff: ActivityCoefficients  = None,
             fugacity_coeff: FugacityCoefficients = None,
             poyinting_coeff: PoyintingCorrectionFactors = None,
             package: Literal['ideal gas','Peng Robinson', 'Dortmund-UNIFAC', 'Peng Robinson | Dortmund-UNIFAC'] = None
         ):
-        chemicals = self.compile_chemicals(skip_checks=skip_checks)
+        # cached chemicals
+        chemicals = self.compiled_chemicals
+
         settings.set_thermo(
             thermo = chemicals,
             mixture = mixture_class.from_chemicals(chemicals),
