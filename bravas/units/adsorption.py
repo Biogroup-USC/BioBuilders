b4d5d96bb33e248f5d0f66f7eefad64cb0b66362
Isaac <isaacleisgarrote@gmail.com>
2026-04-20 08:17:58 +0200

    Add adsorption mass balance

--- bravas/units/adsorption.py
+++ bravas/units/adsorption.py
@@ -2,22 +2,8 @@ import biosteam as bst
 from biosteam.units.design_tools import PressureVessel
 from numba import njit
 import numpy as np
+import math
 from scipy.integrate import solve_ivp
-
-# Code copied from BioSTEAM (https://biosteam.readthedocs.io/), under the University of Illinois/NCSA Open Source License
-# Copyright (c) 2019-2023 BioSTEAM Development Group. All rights reserved.
-@njit(cache=True)
-def fixed_step_odeint(f, x0, step, tf, args):
-    M = int(tf/step)
-    N = len(x0)
-    ts = np.linspace(0, tf, M)
-    xs = np.zeros((M, N))
-    xs[0] = x0
-    for i in range(1, M):
-        dx_dt = f(ts[i], x0, *args)
-        x0 += dx_dt * step
-        xs[i] = x0 
-    return (ts, xs)
 
 @njit
 def equilibrium_loading_Langmuir_isotherm_gas(
@@ -27,63 +13,21 @@ def equilibrium_loading_Langmuir_isotherm_gas(
 ):
     return pi * Kp * q_max / (1 + Kp * pi)  # mol/kg
 
-# Code copied from BioSTEAM (https://biosteam.readthedocs.io/), under the University of Illinois/NCSA Open Source License
-# Copyright (c) 2019-2023 BioSTEAM Development Group. All rights reserved.
 @njit(cache=True)
-def estimate_equilibrium_bed_length(
-        C,  # Solute concentration in the feed [kg / m3]
-        u,  # Superficial velocity [m / h]
-        cycle_time,  # Cycle time [h]
-        rho_adsorbent,  # Bulk density of the bed [kg / m3]
-        q0,  # Equilibrium loading [g / kg]
-    ):
-    q_capacity = q0 * rho_adsorbent / 1000 # kg / m3
-    L = C * u * cycle_time / q_capacity  # m
-    return L
-
-@njit
-def dPdt(
-    t,
-    p,
-    N_slices,
-    Da,
-    dz,
-    isotherm_model,
-    isotherm_args,
-    beta_q0_rho_RT_over_p0,
-    p0,
-    q0,
+def estimate_bed_length_sinnott(
+    C_ads,          # kg/m3
+    u,              # m/h
+    t_ads,          # h
+    rho_adsorbent,  # kg/m3
+    f_L,            # bed usage fraction
+    q_ads,          # g/kg
+    q_regen,        # g/kg
 ):
-    PL = p[:N_slices]
-    qt = p[N_slices:]
+    q_work = (q_ads - q_regen) * rho_adsorbent
+    length = C_ads * u * t_ads / (q_work * f_L)
+    return length
 
-    # Dimensional partial pressure
-    pi_ = PL * p0
-    for i in range(len(pi_)):
-        if pi_[i] < 0.0:
-            pi_[i] = 0.0
-    
-    # Dimensionless equilibrium loading
-    qe = isotherm_model(pi_, *isotherm_args) / q0
-    for i in range(len(qe)):
-        if qe[i] > 1.0:
-            qe[i] = 1.0
-        elif qe[i] < 0.0:
-            qe[i] = 0.0
-    
-    dPL_dz = PL.copy()
-    dPL_dz[1:] = (PL[1:] - PL[:-1]) / dz
-    dPL_dz[0] = (PL[0] - 1.0) / dz
-
-    dq_dt = Da * (qe - qt)
-    dPL_dt = -dPL_dz - beta_q0_rho_RT_over_p0 * dq_dt
-
-    dp_dt = p.copy()
-    dp_dt[:N_slices] = dPL_dt
-    dp_dt[N_slices:] = dq_dt
-    return dp_dt
-
-def adsorption_bed_pressure_drop_gas(
+def ergun_pressure_drop(
         dp,       # particle diameter [m]
         rho,      # gas density [kg/m3]
         mu,       # gas viscosity [Pa*s]
@@ -91,14 +35,14 @@ def adsorption_bed_pressure_drop_gas(
         u,        # superficial velocity [m/s]
         L,        # bed length [m]
     ):
-    one_minus_eps = 1.0 - epsilon
-    eps3 = epsilon**3
     
-    dP = (
-        150.0 * mu * u * one_minus_eps**2 * L / (dp**2 * eps3)
-        + 1.75 * rho * u**2 * one_minus_eps * L / (dp * eps3)
-    )
-    return dP
+    first_term = 150 * ((1-epsilon)**2)/epsilon**3 * (mu * u)/dp**2
+    second_term = 1.75 * (1-epsilon)/epsilon**3 * (rho * u**2)/dp
+
+    dP_L = first_term + second_term
+    dP = dP_L * L
+
+    return dP, dP_L
 
 class GasAdsorptionColumn(PressureVessel, bst.Unit):
     """
@@ -132,20 +76,19 @@ class GasAdsorptionColumn(PressureVessel, bst.Unit):
 
     def _init(
         self,
-        outlet_conc: float = None,
-        cycle_time: float = None,
-        k: float = None,
+        adsorbed_fraction: float = 1.,
+        u_superficial: float = None,
+        t_ads: float = None,
+        t_regen: float = None,
         isotherm_args: tuple = None,
         isotherm_model: str = None,
-        k_regeneration = None,
         regeneration_isotherm_args = None,
         regeneration_isotherm_model = None,
-        LUB_forced = None,
         void_fraction = None,
         rho_adsorbent = None,
         superficial_velocity = None,
-        P = None,
-        p_final_scaled = None,
+        P_ads = None,
+        P_regen = None,
         N_slices = int(50),
         regeneration_fluid = None,
         adsorbate = None,
@@ -153,7 +96,8 @@ class GasAdsorptionColumn(PressureVessel, bst.Unit):
         vessel_type = 'Vertical',
         adsorbent = None,
         N_columns = 3,
-        particle_diameter = None
+        particle_diameter = None,
+        f_L = 0.7,
     ):
         if N_columns not in (2,3):
             raise ValueError('only 2 or 3 columns are valid configurations')
@@ -168,22 +112,29 @@ class GasAdsorptionColumn(PressureVessel, bst.Unit):
             regeneration_pump = self.auxiliary('regeneration_pump', bst.Pump, ins = self.ins[1])
             self.auxiliary('heat_exchanger', bst.HXutility, ins = regeneration_pump.outs[0])
 
-        self.cycle_time = cycle_time
-        self.P = P
+        self.t_ads = t_ads
+        self.t_regen = t_regen
+        self.P_ads = P_ads
         self.adsorbate = adsorbate
-        self.k = k
+        self.adsorbed_fraction = adsorbed_fraction
         self.void_fraction = void_fraction
         self.rho_adsorbent = rho_adsorbent
-        
+        self.f_L = f_L
+        self.u_superficial = u_superficial
+
         self.regeneration_fluid = regeneration_fluid
         self.auxiliary('pump', bst.Pump, ins = self.ins[0])
 
         self.isotherm_model = self.isotherm_models[isotherm_model.lower()]
         self.isotherm_args = isotherm_args
-        
+
+        self.P_regen = P_regen
+        self.regeneration_isotherm_model = self.isotherm_models[regeneration_isotherm_model.lower()]
+        self.regeneration_isotherm_args = regeneration_isotherm_args
+
         self.N_slices = N_slices
         self.superficial_velocity = superficial_velocity
-        self.outlet_mass_fraction = outlet_conc
+        self.adsorbed_fraction = adsorbed_fraction
         self.particle_diameter = particle_diameter
         self.N_columns = N_columns
         self.vessel_material = vessel_material
@@ -194,16 +145,63 @@ class GasAdsorptionColumn(PressureVessel, bst.Unit):
         outlet, spent_fluid, spent_adsorbent = self.outs
         
         adsorbate = self.adsorbate
-        # Outlet composition
+        t_cycle = self.t_regen + self.t_ads
+
+        # Outlets
         outlet.copy_like(feed)
+        spent_fluid.empty()
+        spent_adsorbent.empty()
 
-        adsorbate_in = feed.imass[adsorbate]
-        inert_in = feed.F_mass - adsorbate_in
+        # Inlet adsorbate
+        feed_in = feed.F_mol
+        na_in = feed.imol[adsorbate]
 
-        adsorbate_comp = self.outlet_mass_fraction
-        adsorbate_out = (adsorbate_comp/(1-adsorbate_comp)) * inert_in
+        # Outlet adsorbate
+        na_out = (1-self.adsorbed_fraction) * na_in
+        na_out = min(max(na_out,0.),na_in)
 
-        outlet.imass[adsorbate] = adsorbate_out
+        # Adsorbate removed during adsorption
+        na_removed_rate = max(na_in - na_out, 0.0)
+
+        # Adsorbate removed per column
+        na_removed_cycle = na_removed_rate * self.t_ads
+
+        # Work capacity
+        pa = self._calculate_pi(na_in, feed_in, self.P_ads)
+        q_ads = self.isotherm_model(
+            pa, *self.isotherm_args
+        )
+
+        if self.regeneration_isotherm_model:
+            regen_in = regeneration_fluid.F_mol
+            na_regen = regeneration_fluid.imol[adsorbate]
+            pa_regen = self._calculate_pi(na_regen, regen_in, self.P_regen)
+            q_regen = self.regeneration_isotherm_model(
+                pa_regen, *self.regeneration_isotherm_args
+            )
+        else:
+            q_regen = 0.
+        
+        q_work = max(q_ads - q_regen, 1e-12)
+
+        # Adsorbent mass per column
+        mass_adsorbent = na_removed_cycle / (q_work * self.f_L)
+
+        # Outlet streams
+        outlet.imol[adsorbate] = na_out
+        
+        if self.regeneration:
+            spent_fluid.imol[adsorbate] = na_removed_cycle/t_cycle
+        else:
+            spent_adsorbent.imol[adsorbate] = na_removed_cycle/t_cycle
+        
+        # Variables considered in _design
+        self._M_ads = mass_adsorbent
+        self._q_work = q_work
+        self._nAds_removed_cycle = na_removed_cycle
+        self._q_ads = q_ads
+        self._q_regen = q_regen
+        self._t_cycle = t_cycle
 
     @staticmethod
     def _calculate_pi(ni, nT, P):
@@ -211,104 +209,64 @@ class GasAdsorptionColumn(PressureVessel, bst.Unit):
             return 0.0
         return (ni/nT)*P
 
-    @property
-    def T0(self):
-        return self.ins[0].T
-
-    @property
-    def p0(self):
-        return self._calculate_pi(
-                self.ins[0].imol[self.adsorbate],
-                self.ins[0].F_mol,
-                self.ins[0].P
-            )
-
-    def _PL_to_mass_concentration(self, PL):
-        p = PL * self.p0
-        C_mol = p / (self.R_cte * self.T0)
-        MW = self.ins[0].chemicals[self.adsorbate].MW / 1000.0
-        return C_mol * MW
-
-    def _outlet_mass_concentration_profile(self):
-        PL_out = self.PL_scaled[-1, :]
-        return self._PL_to_mass_concentration(PL_out)
-    
-    def _breakthrough_time_from_mass_fraction(self):
-        feed = self.ins[0]
-        adsorbate = self.adsorbate
-
-        C_out = self._outlet_mass_concentration_profile()
-
-        adsorbate_in = feed.imass[adsorbate]
-        inert_in = feed.F_mass - adsorbate_in
-        w_target = self.outlet_mass_fraction
-        adsorbate_out_target = (w_target/(1.0 - w_target)) * inert_in
-
-    def _simulate_adsorption_bed(
-        self,
-        L,
-        u
-    ):
-        """
-        """
-        cycle_time = self.cycle_time
-        N_slices = self.N_slices
-        void_fraction = self.void_fraction
-        rho_adsorbent = self.rho_adsorbent
-        p0 = self.p0
-        T0 = self.T0
-        k = self.k
-        isotherm_model = self.isotherm_model
-        isotherm_args = self.isotherm_args
-
-        # Initial condition
-        y0 = np.zeros(2*N_slices,dtype=float)
-
-        # Dimensionless groups
-        self.dz = dz = 1.0 / (N_slices - 1.0)
-        self.t_scale = t_scale = L/u
-        self.Da = Da = k * L/u
-
-        # Reference equilibrium loading at inlet conditions [mol/kg]
-        q0 = isotherm_model(p0, *isotherm_args)
-        if q0 <= 0:
-            raise ValueError("Isotherm returned non-positive q0 at inlet conditions.")
+    def _design(self):
+        feed, regeneration_fluid, adsorbent = self.ins
+        design = self.design_results
         
-        self.q0 = q0
+        # Parameters
+        q_ads = self._q_ads
+        q_regen = self._q_regen
+        q_work = self._q_work
 
-        beta = (1.0 - void_fraction) / void_fraction
-        beta_q0_rho_RT_over_p0 = beta * q0 * rho_adsorbent * self.R_cte * T0 / p0
+        n_columns = 3
+        
+        M_ads_col = self._M_ads
+        rho_bulk = self.rho_adsorbent
+        u_superficial = self.u_superficial
+        eps_bed = self.eps_bed
+        f_L = self.f_L
+        dp = self.particle_diameter
 
-        args = (
-            N_slices,
-            Da,
-            dz,
-            isotherm_model,
-            isotherm_args,
-            beta_q0_rho_RT_over_p0,
-            p0,
-            q0
+        gas_viscosity = self.gas_viscosity
+        gas_density = self.gas_density
+
+        # Adsorbate concentration
+        Q_feed = feed.F_vol
+        C_ads = feed.imol[self.adsorbate] / Q_feed
+
+        # Column length
+        L_col = estimate_bed_length_sinnott(
+            C_ads = C_ads,
+            u = u_superficial,
+            t_ads = self.t_ads,
+            rho_adsorbent = rho_bulk,
+            f_L = f_L,
+            q_ads = q_ads,
+            q_regen = q_regen,
         )
 
-        # Integrate from tau = 0 to tau = cycle_time / t_scale
-        tf = cycle_time / t_scale
+        # Bed volume per column
+        V_bed_col = M_ads_col / rho_bulk
 
-        sol = solve_ivp(
-            fun = dPdt,
-            t_span = (0.0, tf),
-            y0 = y0,
-            method = 'BDF',
-            args = args,
-            atol = 1e-8,
-            rtol = 1e-6,
+        # Area and diameter
+        A_col = Q_feed / u_superficial
+        D_col = math.sqrt(4.0 * A_col / math.pi)
+
+        # Calculate pressure drop
+        dP, dP_L = ergun_pressure_drop(
+            dp = dp,
+            rho = gas_density,
+            mu = gas_viscosity,
+            epsilon = eps_bed,
+            u = u_superficial,
+            L = L_col,
         )
 
-        if not sol.success:
-            raise RuntimeError(f"Bed simulation failed: {sol.message}")
-        
-        # Store results
-        self.t_scaled = sol.t
-        self.PL_scaled = sol.y[:N_slices, :]
-        self.q_scaled = sol.y[N_slices:, :]
+        # Results
+        design["Adsorbent per column"] = M_ads_col
+        design["Number of columns"] = n_columns
+        design["Total adsorbent"] = M_ads_col * n_columns
 
-        return sol
+
+    def _cost(self):
+        
