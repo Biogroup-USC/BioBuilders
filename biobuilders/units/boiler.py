697aacf8a588dd23ca54de2cbc3b5b4cbe31b22c
Isaac <isaacleis.garrote@usc.es>
2026-06-30 14:22:47 +0200

    Add Q_required calculation to NaturalGasBoiler

--- biobuilders/units/boiler.py
+++ biobuilders/units/boiler.py
@@ -1,4 +1,32 @@
 import biosteam as bst
 
-class CombustionBoiler(bst.Boiler):
-    """"""
+class NaturalGasBoiler(bst.Unit): 
+    """
+    """
+    _N_ins = 3
+    _N_outs = 2
+
+    def _init(
+        self,
+        excess_air: float = 0.20,
+        hot_fluid_T: float = 273.15 + 60.0,
+        thermal_efficiency: float = 0.85,
+    ):
+        """
+        """
+        self.excess_air = excess_air
+        self.hot_fluid_T = hot_fluid_T
+        self.thermal_efficiency = thermal_efficiency
+
+    def _run(self):
+        
+        natural_gas, combustion_air, cold_fluid = self.ins
+        flue_gas, hot_fluid = self.outs
+
+        # Calculate required duty
+        hot_fluid.copy_like(cold_fluid)
+        hot_fluid.T = self.hot_fluid_T
+
+        Q_required = hot_fluid.H - cold_fluid.H
+        
+    
